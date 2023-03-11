8a440b71d569d4d4568058ab75340ca3934962dc
Chris D <faithoflifedev@gmail.com>
2023-03-11 09:25:11 -0500

    v1.0.7+6

--- Formula/vision.rb
+++ Formula/vision.rb
@@ -1,8 +1,8 @@
 class Vision < Formula
   desc "Allows you to add Google Visions image labeling, face, logo, and landmark detection, OCR, and detection of explicit content, into applications."
   homepage "https://github.com/faithoflifedev/google_vision"
-  url "https://api.github.com/repos/faithoflifedev/google_vision/tarball/v1.0.7+4"
-  sha256 "98cb6653ace12615f323693f4361a08b4f6815691e7548aeffb6a94ef97df7f9"
+  url "https://api.github.com/repos/faithoflifedev/google_vision/tarball/v1.0.7+6"
+  sha256 "88872113dcc4eb6ce3a700a9791ac7c3e385317510e258917b122b1f9afa62cc"
   license "MIT"
   
   depends_on "dart-lang/dart/dart" => :build
@@ -14,6 +14,6 @@ class Vision < Formula
   end
   
   test do
-    assert_match "obs_websocket v1.0.7+4", shell_output("bin/vision version")
+    assert_match "obs_websocket v1.0.7+6", shell_output("bin/vision version")
   end
 end
